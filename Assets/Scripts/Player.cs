538445aacdc02f029dab5e57a6596f2fdf7b6483
BottaDev <fedebotta@gmail.com>
2020-03-28 15:42:58 -0300

    Bomb functionality

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float bombCd;
+    [Range(min: 0.5f, max: 3f)]
+    public float bombTimeToExplode = 3f;
+    [Range(min: 2, max: 8)]
+    public int bombRange = 2;   // Valor expresado en bloques del mapa
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
