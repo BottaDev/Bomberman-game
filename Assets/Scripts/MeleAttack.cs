91a799d2d2f3675ddd2cc55e27f7e2ac82383b45
Juli520 <50522986+Juli520@users.noreply.github.com>
2020-04-06 18:57:44 -0300

    Mele

---
+++ Assets/Scripts/MeleAttack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleAttack : MonoBehaviour
+{
+    public Transform positionAttack;
+    public float rangeAttack = 0.5f;
+    public LayerMask allLayers;
+    public float damage;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(positionAttack.position, rangeAttack, allLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Le di");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (positionAttack == null)
+            return;
+
+        Gizmos.DrawWireSphere(positionAttack.position, rangeAttack);
+    }
+}
