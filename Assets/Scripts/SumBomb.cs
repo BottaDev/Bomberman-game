a52d901a216c64c2d100895baffe38b71c722d26
Juli520 <50522986+Juli520@users.noreply.github.com>
2020-04-14 19:37:36 -0300

    PowerUps

---
+++ Assets/Scripts/SumBomb.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumBomb : MonoBehaviour
+{
+    public int extraBomb = 3;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+            PickUp(collision);
+    }
+
+    void PickUp(Collider2D player)
+    {
+        Player bomb = player.GetComponent<Player>();
+
+        bomb.ApplyPowerUp(extraBomb);
+
+        Destroy(gameObject);
+    }
+}
