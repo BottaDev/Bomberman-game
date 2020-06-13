8fa0a3ff4ac5c40642530f3b15b1a83fc21d1af7
BottaDev <fedebotta@gmail.com>
2020-06-13 20:59:56 -0300

    New Gui Menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject playMenu;
+    public GameObject creditsMenu;
+
+    // Setea la cantidad de players en la partida
+    public void SetPlayers(int numberOfPlayers)
+    {
+        GameManager.instance.SetPlayers(numberOfPlayers);
+
+        SceneManager.LoadScene(1);
+    }
+
+    // Setea el enabled del menu PlayManu
+    public void SetPlayMenu()
+    {
+        creditsMenu.SetActive(false);
+        playMenu.SetActive(!playMenu.activeSelf);
+    }
+
+    // Setea el enabled del menu CreditsMenu
+    public void SetCreditsMenu()
+    {
+        playMenu.SetActive(false);
+        creditsMenu.SetActive(!creditsMenu.activeSelf);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
