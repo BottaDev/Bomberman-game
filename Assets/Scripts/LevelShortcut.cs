30731ae209520ec84a18618f412b24adc7503a08
BottaDev <fedebotta@gmail.com>
2020-07-01 16:57:53 -0300

    Level Skip

---
+++ Assets/Scripts/LevelShortcut.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelShortcut : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+            SceneManager.LoadScene(1);
+        else if (Input.GetKeyDown(KeyCode.F2))
+            SceneManager.LoadScene(2);
+        else if (Input.GetKeyDown(KeyCode.F3))
+            SceneManager.LoadScene(3);
+        else if (Input.GetKeyDown(KeyCode.F4))
+            SceneManager.LoadScene(4);
+        else if (Input.GetKeyDown(KeyCode.F5))
+            SceneManager.LoadScene(5);
+    }
+}
